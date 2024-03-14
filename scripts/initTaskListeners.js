008193bc1ea6fef95e25426b737134ab67f292da
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-03-14 13:45:59 +0200

    Add sortablejs library and initialize Sortable on task list

--- scripts/initTaskListeners.js
+++ scripts/initTaskListeners.js
@@ -1,3 +1,4 @@
+import Sortable from 'sortablejs';
 import {
   TaskListElement, getTaskListCheckboxElements, getTaskListDeleteElements, getTasksElements,
 } from './Elements';
@@ -14,39 +15,54 @@ export const initTaskListeners = () => {
       box.addEventListener('keydown', (e) => e.key === 'Enter' && toggleATask(e, index));
     }, 0);
   });
-  getTasksElements().forEach((task) => {
-    task.addEventListener('dragstart', () => {
-      //  Adding dragging class after a delay
-      setTimeout(() => task.classList.add('dragging'), 0);
-    });
 
-    // Adding touch events for mobile support
-    task.addEventListener('touchstart', () => {
-      // Adding dragging class after a delay
-      setTimeout(() => task.classList.add('dragging'), 0);
-    });
+  //   getTasksElements().forEach((task) => {
+  //     task.addEventListener('dragstart', () => {
+  //       //  Adding dragging class after a delay
+  //       setTimeout(() => task.classList.add('dragging'), 0);
+  //     });
 
-    // Removing dragging on dragend event
-    task.addEventListener('dragend', () => {
-      task.classList.remove('dragging');
-    });
+  //     // Adding touch events for mobile support
+  //     task.addEventListener('touchstart', () => {
+  //       // Adding dragging class after a delay
+  //       setTimeout(() => task.classList.add('dragging'), 0);
+  //     });
 
-    // Removing dragging on touchend event
-    task.addEventListener('touchend', () => {
-      task.classList.remove('dragging');
-    });
-  });
+  //     // Removing dragging on dragend event
+  //     task.addEventListener('dragend', () => {
+  //       task.classList.remove('dragging');
+  //     });
 
-  const initSortableList = (e) => {
-    e.preventDefault();
-    const draggingItem = TaskListElement.querySelector('.dragging');
-    const siblings = [...TaskListElement.querySelectorAll('.TaskList__TaskContent:not(.dragging)')];
+  //     // Removing dragging on touchend event
+  //     task.addEventListener('touchend', () => {
+  //       task.classList.remove('dragging');
+  //     });
+  //   });
 
-    const nextSibling = siblings.find((sibling) => e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2);
-    TaskListElement.insertBefore(draggingItem, nextSibling);
-  };
+  //   const initSortableList = (e) => {
+  //     e.preventDefault();
+  //     const draggingItem = TaskListElement.querySelector('.dragging');
+  //     const siblings = [...TaskListElement.querySelectorAll('.TaskList__TaskContent:not(.dragging)')];
 
-  TaskListElement.addEventListener('dragover', initSortableList);
-  TaskListElement.addEventListener('dragenter', (e) => e.preventDefault());
-  TaskListElement.addEventListener('drop', initSortableList);
-}; export default initTaskListeners;
+  //     const nextSibling = siblings.find((sibling) => e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2);
+  //     TaskListElement.insertBefore(draggingItem, nextSibling);
+  //   };
+
+  //   TaskListElement.addEventListener('dragover', initSortableList);
+  //   TaskListElement.addEventListener('dragenter', (e) => e.preventDefault());
+  //   TaskListElement.addEventListener('drop', initSortableList);
+  // };
+
+  // Initialize Sortable on your list
+
+  const sortable = Sortable.create(TaskListElement);
+  // var sortable = Sortable.create(TaskListElement, {
+  //   onStart: function (evt) {
+  //     // Custom code to run when a drag starts
+  //   },
+  //   onEnd: function (evt) {
+  //     // Custom code to run when a drag ends
+  //   },
+  // });
+};
+export default initTaskListeners;
